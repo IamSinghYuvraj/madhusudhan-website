4d0053697e834483a8e529155c902d5d009c3b3d
Yuvraj Singh <121106713+IamSinghYuvraj@users.noreply.github.com>
2025-08-06 21:35:44 +0530

    adding resend

--- app/api/sendMessage/route.ts
+++ app/api/sendMessage/route.ts
@@ -1,30 +1,21 @@
 "use server";
-import { NextRequest, NextResponse } from 'next/server';
-import nodemailer from 'nodemailer';
+import { NextResponse } from 'next/server';
+import { Resend } from 'resend';
+
+const resend = new Resend(process.env.RESEND_API_KEY);
 
 export async function POST(request: Request) {
   try {
     const body = await request.json();
     const { name, email, phone, message } = body;
 
-    // Send email
-    const transporter = nodemailer.createTransport({
-      service: 'gmail',
-      auth: {
-        user: process.env.EMAIL_USER ,
-        pass: process.env.EMAIL_PASSWORD,
-      },
-    });
-
-    const mailOptions = {
-      from: process.env.EMAIL_USER,
-      to: process.env.EMAIL_RECEIVER ,
+    await resend.emails.send({
+      from: process.env.EMAIL_USER!,
+      to: process.env.EMAIL_RECEIVER!,
       subject: 'New Query',
       text: `Name: ${name}\nEmail: ${email}\nPhone: ${phone}\nMessage: ${message}`,
-    };
+    });
 
-    await transporter.sendMail(mailOptions);
-    
     return NextResponse.json({ message: 'Message sent successfully' }, { status: 200 });
   } catch (error) {
     console.error('Error processing request:', error);
