1a9c4e084b0095a1f2f8559460cf47064879baea
Yuvraj Singh <121106713+IamSinghYuvraj@users.noreply.github.com>
2025-08-06 21:08:01 +0530

    .

--- app/api/sendMessage/route.ts
+++ app/api/sendMessage/route.ts
@@ -10,14 +10,14 @@ export async function POST(request: Request) {
     const transporter = nodemailer.createTransport({
       service: 'gmail',
       auth: {
-        user: process.env.EMAIL_USER || 'yuvraj.singh.06052007@gmail.com',
-        pass: process.env.EMAIL_PASSWORD || 'whxq swcw adon pkhz',
+        user: process.env.EMAIL_USER ,
+        pass: process.env.EMAIL_PASSWORD,
       },
     });
 
     const mailOptions = {
-      from: process.env.EMAIL_USER || 'developeruv07@gmail.com',
-      to: process.env.EMAIL_RECEIVER || 'developeruv07@gmail.com',
+      from: process.env.EMAIL_USER,
+      to: process.env.EMAIL_RECEIVER ,
       subject: 'New Query',
       text: `Name: ${name}\nEmail: ${email}\nPhone: ${phone}\nMessage: ${message}`,
     };
